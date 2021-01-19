afc34961e3067839b733a44ba54a6c8ac3090403
poday98 <poday736.98@gmail.com>
2021-01-19 11:15:07 -0600

    redes

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Fragment } from 'react'
 
-import { Grid, Box, makeStyles } from '@material-ui/core'
+import { Grid, Box, makeStyles, Typography, Link } from '@material-ui/core';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import FacebookIcon from '@material-ui/icons/Facebook';
 
 const useStyles = makeStyles((theme) => ({
 
@@ -24,8 +26,24 @@ export default function Footer() {
                         alt="Tecnologia de Huawei" 
                     />
                </Box>
-               <Box>
-
+               <Box align="center">
+                   <Typography>
+                       Telefono: 3173873462
+                   </Typography>
+                   <Typography >
+                       Javier Mina #452, Coto privado Javier Mina, CP 48900
+                   </Typography>
+                   <Typography>
+                       Autlan de Navarro, Jalisco, 
+                   </Typography>
+               </Box>
+               <Box align="center">
+                    <Link href="https://www.facebook.com/cafi.tutiendaenlinea?_rdc=1&_rdr" color="inherit" target="_blank">
+                        <FacebookIcon  fontSize="large"/>
+                    </Link>
+                    <Link href="https://www.instagram.com/cafi.tutiendaonline/" color="inherit" target="_blank">
+                        <InstagramIcon fontSize="large"/>
+                    </Link>
                </Box>
            </Grid>
        </Fragment>
